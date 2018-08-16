0ee8dee31704ddefeaf56c4f4b50320164895906
Lucas Dohmen <lucas@dohmen.io>
2018-08-16 15:27:24 +0200

    Put CLI into the SecurityReport module

--- lib/security_report/cli.rb
+++ lib/security_report/cli.rb
@@ -1,49 +1,51 @@
 require 'security_report'
 
-class CLI
-  attr_accessor :format
+module SecurityReport
+  class CLI
+    attr_accessor :format
 
-  def initialize
-    @format = "plain"
-  end
+    def initialize
+      @format = "plain"
+    end
 
-  def help
-    puts <<~HELPTEXT
-      security_report [options] files...
+    def help
+      puts <<~HELPTEXT
+        security_report [options] files...
 
-      -h, --help
-        Print this message
-      --format
-        Provide the format: plain or table (default: plain)
-      --update
-        Update the vulnerability database before running
-    HELPTEXT
-    exit
-  end
+        -h, --help
+          Print this message
+        --format
+          Provide the format: plain or table (default: plain)
+        --update
+          Update the vulnerability database before running
+      HELPTEXT
+      exit
+    end
 
-  def reporter
-    case format
-    when "plain"
-      require 'security_report/plain_reporter'
-      @reporter = SecurityReport::PlainReporter.new
-    when "table"
-      require 'security_report/table_reporter'
-      @reporter = SecurityReport::TableReporter.new
-    else
-      puts "Unknown format '#{format}'"
+    def reporter
+      case format
+      when "plain"
+        require 'security_report/plain_reporter'
+        @reporter = PlainReporter.new
+      when "table"
+        require 'security_report/table_reporter'
+        @reporter = TableReporter.new
+      else
+        puts "Unknown format '#{format}'"
+        exit 1
+      end
+    end
+
+    def update
+      return if Database.update
+      puts "Failed updating database!"
       exit 1
     end
-  end
 
-  def update
-    return if SecurityReport::Database.update
-    puts "Failed updating database!"
-    exit 1
-  end
-
-  def run(files)
-    results = SecurityReport::Auditor.audit(files)
-    reporter.report(results)
-    exit 1 if results.any?
+    def run(files)
+      results = Auditor.audit(files)
+      reporter.report(results)
+      exit 1 if results.any?
+    end
   end
 end
